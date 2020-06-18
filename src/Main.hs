6717d57b66177005f7cd30a570bfa0650986b832
Trevor Thai Kim Nguyen <trevortknguyen@utexas.edu>
2020-06-17 22:21:15 -0700

    Display a static webpage

--- src/Main.hs
+++ src/Main.hs
@@ -5,11 +5,17 @@ import Web.Scotty
 
 import Data.Monoid (mconcat)
 import Data.Text.Lazy (Text)
- 
+
+import Control.Monad.IO.Class (liftIO)
+
+import qualified Data.ByteString.Lazy as BS (readFile)
+
+import Data.Text.Lazy.Encoding (decodeUtf8)
 
 getWebpage :: ActionM Text
 getWebpage = do
-    return "<h1>Scotty!</h1>"
+    content <- liftIO $ BS.readFile "html/index.html"
+    return $ decodeUtf8 content
 
 main :: IO ()
 main = scotty 3000 $
