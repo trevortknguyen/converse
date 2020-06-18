a9153a324867b3bed6d9f009d2d6cb56c0b7ce16
Trevor Thai Kim Nguyen <trevortknguyen@utexas.edu>
2020-06-17 22:31:06 -0700

    Make HtmlWebpage refactor

--- src/Main.hs
+++ src/Main.hs
@@ -3,21 +3,8 @@ module Main where
 
 import Web.Scotty
 
-import Data.Monoid (mconcat)
-import Data.Text.Lazy (Text)
-
-import Control.Monad.IO.Class (liftIO)
-
-import qualified Data.ByteString.Lazy as BS (readFile)
-
-import Data.Text.Lazy.Encoding (decodeUtf8)
-
+import HtmlWebpage
 import ChatServer
-
-getWebpage :: ActionM Text
-getWebpage = do
-    content <- liftIO $ BS.readFile "html/index.html"
-    return $ decodeUtf8 content
 
 main :: IO ()
 main = scotty 3000 $
